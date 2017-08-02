f6ad09e35399fd9c5dbefbac592e21cee8a6201a
Junjie Guan <jack2684@users.noreply.github.com>
2017-08-01 17:31:35 -0700

    Create config.fish

---
+++ config.fish
@@ -0,0 +1,9 @@
+# ~/.config/fish/config.fish
+alias gits "git status"
+alias gitc "git commit"
+alias gitl 'git log --pretty=format:"%h - %an, %ar : %s"'
+alias gitb "git branch"
+alias gitco "git checkout"
+alias gitca "git commit --amend"
+alias gitpm "git pull origin master --rebase"
+alias gitpf "git push -f"
